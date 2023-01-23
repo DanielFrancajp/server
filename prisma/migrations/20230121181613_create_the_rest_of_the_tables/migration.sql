0ca0c3991d5ce91b5c13392fd030ded7dace0e7f
Daniel França <daniel.franca@secom.local>
2023-01-23 15:00:10 -0300

    fix error tabelas

--- prisma/migrations/20230121181613_create_the_rest_of_the_tables/migration.sql
+++ prisma/migrations/20230121181613_create_the_rest_of_the_tables/migration.sql
@@ -15,14 +15,15 @@ CREATE TABLE "days" (
 CREATE TABLE "day_habits" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "day_id" TEXT NOT NULL,
-    "habit_id" TEXT NOT NULL
+    "habit_id" TEXT NOT NULL,
+    CONSTRAINT "day_habits_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "days" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
 
 -- CreateIndex
-CREATE INDEX "days_date_idx" ON "days"("date");
+CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "day_habits_day_id_habit_id_key" ON "day_habits"("day_id", "habit_id");
