2dad9e3dbe2a7e9156d4cdad5c5a2a5027acf1fd
DanielFrancajp <danieldenotas@gmail.com>
2023-01-28 12:44:16 -0300

    Finalização do back-end

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import fastify from "fastify";
-import cors from '@fastify/cors'
-import { appRoutes } from "./lib/routes";
 
-const app = fastify()
+import cors from "@fastify/cors"
+import Fastify from 'fastify'
+import { appRoutes } from "./routes"
 
+const app = Fastify()
 
 app.register(cors)
 app.register(appRoutes)
 
 
-
-app.listen({
-    port: 3333
+app.listen({ 
+  port: 3333 
 }).then(() => {
-    console.log('HTTP Server running!')
+  console.log("HTTP Server running!")
 })
